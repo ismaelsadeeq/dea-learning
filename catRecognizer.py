8d9255cc8168f8a3c0c2aceb4901dc13aeb59b9e
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2021-09-14 09:56:57 +0100

    importing libraries

--- catRecognizer.py
+++ catRecognizer.py
@@ -4,4 +4,4 @@ import h5py
 import scipy
 from PIL import Image
 from scipy import ndimage
-# from lr_utils import load_dataset
+from lr_utils import load_dataset
--- neuralNetwork.py
+++ neuralNetwork.py
@@ -3,3 +3,6 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 from tensorflow import keras
 
+fashion_mnist = keras.datasets.fashion_mnist # loading the dataset of images in pixels
+
+(train_images,train_labels), (test_images, test_labels) = fashion_mnist.load_data() # split the data into testing and training
